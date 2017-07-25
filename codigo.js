0cbc9ffcd883056c0df3ca08b2ca5b599944d293
Alexa <alexadiaz@msn.com>
2017-07-25 14:57:55 -0400

    create function comenzar

--- codigo.js
+++ codigo.js
@@ -1,11 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var ingresar_productoInput = getId("ingresar_producto");
-    var ingresar_cantidadInput = getId("ingresar_cantidad");
-    var contenidoUl = getId("contenido");
-
-    ingresar_productoInput.addEventListener("click",function(){
-        crear_contenido(ingresar_productoInput,ingresar_cantidadInput,contenidoUl);
-    });
+    comenzar();
 });
 
 function getId(id){
@@ -16,16 +10,30 @@ function createElement(elemento){
     return document.createElement(elemento);
 }
 
+function comenzar(){
+    var ingresar_productoInput = getId("ingresar_producto");
+    var ingresar_cantidadInput = getId("ingresar_cantidad");
+    var contenidoUl = getId("contenido");
+
+    ingresar_productoInput.addEventListener("click",function(){
+        crear_contenido(ingresar_productoInput,ingresar_cantidadInput,contenidoUl);
+    });
+}
+
 function crear_contenido(producto,cantidad,ul){
     var elementoLi = createElement("li");
+    
     var elemento_productoSpan = createElement("span");
     var elemento_cantidadSpan = createElement("span");
-
-    elemento_productoSpan.textContent=producto.value;
-    elemento_cantidadSpan.textContent=cantidad.value;
+    var elemento_unitarioSpan = createElement("span");
+    var elemento_totalSpan = createElement("span");
 
     elementoLi.appendChild(elemento_productoSpan);
     elementoLi.appendChild(elemento_cantidadSpan);
+    elementoLi.appendChild(elemento_unitarioSpan);
+    elementoLi.appendChild(elemento_totalSpan);
 
     ul.appendChild(elementoLi);
+    
+    guardar_contenido(producto,cantidad);
 }
