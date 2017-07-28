e21ab3b7af33bfa98291bd0749cbf721a2e535dd
Alexa <alexadiaz@msn.com>
2017-07-28 17:52:22 -0400

    crear primero variable y luego html

--- codigo.js
+++ codigo.js
@@ -16,7 +16,7 @@ function propiedades_elementosLi(elemento,contenido){
     elemento.className="js_margen_contenido";
     switch(elemento.nodeName){
         case "SPAN":
-            elemento.textContent=contenido.value;
+            elemento.textContent=contenido;
             break;
         case "DIV":
             elemento.innerText=contenido;
@@ -44,7 +44,8 @@ function comenzar(){
          }
          if (keyname === "Enter"){
             if (ingresar_productoInput.value !== "" && ingresar_cantidadInput.value !==""){
-                crear_contenidoUl(ingresar_productoInput,ingresar_cantidadInput,contenidoUl);
+                guardar_contenidoUl(ingresar_productoInput,ingresar_cantidadInput);
+                crear_contenidoUl(contenidoUl);
                 ingresar_productoInput.value="";
                 ingresar_productoInput.focus();
                 ingresar_cantidadInput.value="";
@@ -58,48 +59,48 @@ function comenzar(){
     });    
 }
 
+var contenidoLi=[];
 var contador=0;
-function crear_contenidoUl(producto,cantidad,ul){
-    var elementoLi = create_pegar_elementosUl("li",ul);
-    elementoLi.data = contador += 1;
-    
-    var elemento_productoSpan = create_pegar_elementosUl("span",elementoLi);
-    propiedades_elementosLi(elemento_productoSpan,producto);
-    
-    var elemento_cantidadSpan = create_pegar_elementosUl("span",elementoLi);
-    propiedades_elementosLi(elemento_cantidadSpan,cantidad);
-    
-    var elemento_unitarioSpan = create_pegar_elementosUl("span",elementoLi);
-    propiedades_elementosLi(elemento_unitarioSpan,producto);
-    
-    var elemento_totalSpan = create_pegar_elementosUl("div",elementoLi);
-    var valor_total = calcular_valor_total(elemento_cantidadSpan);
-    propiedades_elementosLi(elemento_totalSpan,valor_total);
-
-    var elemento_modificarInput = create_pegar_elementosUl("input",elementoLi);
-    propiedades_elementosLi(elemento_modificarInput,"Modificar");
-    elemento_modificarInput.addEventListener("click",function(){
-        modificar_contenidoLi(this.parentElement);
-    });
-
-    var elemento_eliminarInput = create_pegar_elementosUl("input", elementoLi);
-    propiedades_elementosLi(elemento_eliminarInput,"Eliminar");
-    elemento_eliminarInput.addEventListener("click",function(){
-        eliminar_contenidoLi(this.parentElement);
-    });
-    
-    guardar_contenidoUl(elementoLi);
+function guardar_contenidoUl(producto,cantidad){
+    contenidoLi.push({
+        id: contador += 1,
+        articulo: producto.value,
+        unidades:cantidad.value,
+        valor_unitario: 10,
+        valor_total: calcular_valor_total(cantidad.value)
+    }); 
+    console.log(contenidoLi);
 }
 
-var contenidoLi=[];
-function guardar_contenidoUl(li){
-    contenidoLi.push({
-        id: li.data,
-        articulo: li.children[0].textContent,
-        cantidad: li.children[1].textContent,
-        valor_unitatio: li.children[2].textContent,
-        valor_total: li.children[3].textContent
-    }); 
+function crear_contenidoUl(ul){
+    for(var i in contenidoLi){
+        var elementoLi = create_pegar_elementosUl("li",ul);
+        elementoLi.data = contenidoLi[i].id;
+
+        var elemento_productoSpan = create_pegar_elementosUl("span",elementoLi);
+        propiedades_elementosLi(elemento_productoSpan,contenidoLi[i].articulo);
+
+        var elemento_cantidadSpan = create_pegar_elementosUl("span",elementoLi);
+        propiedades_elementosLi(elemento_cantidadSpan,contenidoLi[i].unidades);
+
+        var elemento_unitarioSpan = create_pegar_elementosUl("span",elementoLi);
+        propiedades_elementosLi(elemento_unitarioSpan,contenidoLi[i].valor_unitario);
+
+        var elemento_totalSpan = create_pegar_elementosUl("div",elementoLi);
+        propiedades_elementosLi(elemento_totalSpan,contenidoLi[i].valor_total);
+
+        var elemento_modificarInput = create_pegar_elementosUl("input",elementoLi);
+        propiedades_elementosLi(elemento_modificarInput,"Modificar");
+        elemento_modificarInput.addEventListener("click",function(){
+            modificar_contenidoLi(this.parentElement);
+        });
+
+        var elemento_eliminarInput = create_pegar_elementosUl("input", elementoLi);
+        propiedades_elementosLi(elemento_eliminarInput,"Eliminar");
+        elemento_eliminarInput.addEventListener("click",function(){
+            eliminar_contenidoLi(this.parentElement);
+        }); 
+    }   
 }
 
 function modificar_contenidoLi(li){
@@ -140,7 +141,7 @@ function nueva_info(li){
 }
 
 function calcular_valor_total(cantidad){
-    var calculo = parseInt(cantidad.textContent)*10;
+    var calculo = parseInt(cantidad)*10;
     calcular_total_factura(calculo);
     return "$ " + new Intl.NumberFormat("es-CO").format(calculo);
 }
